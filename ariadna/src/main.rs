92b937b2e1aaa55d58d4889297261344bf5a0bc2
Igor Sidorov <igor.cug@gmail.com>
2026-02-03 09:29:05 +0200

    feature: introduce wokwi for esp32 s3 simulation

--- ariadna/src/main.rs
+++ ariadna/src/main.rs
@@ -1,13 +1,38 @@
+use std::thread;
+use std::time::Duration;
+
+#[cfg(target_arch = "xtensa")]
+use esp_idf_hal::gpio::PinDriver;
+#[cfg(target_arch = "xtensa")]
+use esp_idf_hal::peripherals::Peripherals;
+
 fn main() {
     #[cfg(target_arch = "xtensa")]
     {
-        // It is necessary to call this function once. Otherwise some patches to the
-        // runtime implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
         esp_idf_svc::sys::link_patches();
-
-        // Bind the log crate to the ESP Logging facilities
         esp_idf_svc::log::EspLogger::initialize_default();
+
+        log::info!("Starting LED blink demo");
+
+        let peripherals = Peripherals::take().expect("Failed to take peripherals");
+        let mut led = PinDriver::output(peripherals.pins.gpio4).expect("Failed to init LED pin");
+
+        let mut on = false;
+        loop {
+            on = !on;
+            if on {
+                log::info!("LED ON");
+                led.set_high().unwrap();
+            } else {
+                log::info!("LED OFF");
+                led.set_low().unwrap();
+            }
+            thread::sleep(Duration::from_millis(500));
+        }
     }
 
-    log::info!("Hello, world!");
+    #[cfg(not(target_arch = "xtensa"))]
+    {
+        log::info!("This code is meant to run on ESP32");
+    }
 }
